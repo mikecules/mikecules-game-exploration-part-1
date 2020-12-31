074529293e7dcf330879a31a32eb38b4248ed9bb
Michael Moncada <michael.moncada@gmail.com>
2020-12-30 21:34:16 -0500

    wired up some sprites

--- MeteorGame/main.py
+++ MeteorGame/main.py
@@ -1,16 +1,60 @@
-# This is a sample Python script.
-
-# Press ⇧F10 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import pygame
+import sys
+import sprites
+import random
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def get_spaceship(screen):
+    return sprites.SpaceShip(screen, 'spaceship.png', 640, 500, 10)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def get_meteors(screen, number_of_meteors):
+    meteors = []
+    screen_width = screen.get_width()
+
+    for i in range(number_of_meteors):
+        meteor_index = random.randrange(1, 4)
+        meteor_file = 'Meteor{0}.png'.format(meteor_index)
+        x_pos = random.randrange(0, screen_width)
+        x_direction = 1 if random.randint(0, 1) == 0 else -1
+        x_speed = x_direction * (0.25 + random.randrange(1, 5) * random.random())
+        y_speed = 1 + random.randrange(1, 5) * random.random()
+
+        meteor = sprites.Meteor(screen, meteor_file, x_pos, 0, x_speed, y_speed)
+        meteors.append(meteor)
+
+    return meteors
+
+
+def start_game(window_width=1200, window_height=720):
+    pygame.init()
+    screen = pygame.display.set_mode((window_width, window_height))
+    clock = pygame.time.Clock()
+
+    spaceship = get_spaceship(screen)
+    spaceship_group = pygame.sprite.GroupSingle(spaceship)
+
+    meteors = get_meteors(screen, 10)
+    meteor_group = pygame.sprite.Group(meteors)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        screen.fill((42, 45, 51))
+
+        spaceship_group.draw(screen)
+        spaceship.update()
+
+        meteor_group.draw(screen)
+
+        for meteor in meteors:
+            meteor.update()
+
+        pygame.display.update()
+        clock.tick(120)
+
+
+start_game()
