e43cebdce90237c64daccd80c6870690b52a0d0c
Michael Moncada <michael.moncada@gmail.com>
2020-12-30 22:09:08 -0500

    Added reuse count as TTL to expire older meteors

--- MeteorGame/sprites/Meteor.py
+++ MeteorGame/sprites/Meteor.py
@@ -1,5 +1,4 @@
 from .Sprite import Sprite
-import random
 
 
 class Meteor(Sprite):
@@ -7,6 +6,7 @@ class Meteor(Sprite):
         Sprite.__init__(self, screen, file_name, x_position, y_position)
         self.x_speed = x_speed
         self.y_speed = y_speed
+        self.sprite_reused_count = 0
 
     def update(self):
         x1, y1 = self.rect.center
@@ -20,12 +20,19 @@ class Meteor(Sprite):
         screen_height = self._screen.get_height()
         width = self.image.get_width()
         height = self.image.get_height()
+        updated = False
 
         if self.rect.left >= screen_width:
             self.rect.right = 0
+            updated = True
         elif self.rect.right <= 0:
-            self.rect.left = screen_width + width /2
+            self.rect.left = screen_width + width / 2
+            updated = True
 
         if self.rect.top >= screen_height:
             self.rect.bottom = -height / 2
+            updated = True
+
+        if updated:
+            self.sprite_reused_count += 1
 
