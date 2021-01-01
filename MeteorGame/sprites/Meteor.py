6fcd4f351a6250f4a93ae35481a581f536aeb637
Michael Moncada <michael.moncada@gmail.com>
2021-01-01 09:28:27 -0500

    Add meteor rotation

--- MeteorGame/sprites/Meteor.py
+++ MeteorGame/sprites/Meteor.py
@@ -1,4 +1,5 @@
 from .Sprite import Sprite
+import random
 
 
 class Meteor(Sprite):
@@ -7,13 +8,30 @@ class Meteor(Sprite):
         self.x_speed = x_speed
         self.y_speed = y_speed
         self.sprite_reused_count = 0
+        self.original_image = self.image.copy()
+        self.z_rotate_speed = min(0.1, random.random()) if random.randrange(0, 2) > 0 else max(-0.1, -random.random())
+        self.current_z_angle = 0
         self.max_reuse_count = 0
+
+    def rotate(self, x, y):
+        z_angle = self.current_z_angle + self.z_rotate_speed
+
+        if z_angle >= 360:
+            z_angle -= 360
+        elif z_angle <= -360:
+            z_angle += 360
+
+        self.image = self._pygame.transform.rotate(self.original_image, self.current_z_angle)
+        self.rect = self.image.get_rect(center=(x, y))
+        self.current_z_angle = z_angle
 
     def update(self):
         x1, y1 = self.rect.center
         x2 = x1 + self.x_speed
         y2 = y1 + self.y_speed
-        self.rect.center = (x2, y2)
+
+        self.rotate(x2, y2)
+
         self.apply_screen_constraints()
 
     def apply_screen_constraints(self):
