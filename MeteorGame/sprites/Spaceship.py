40ba8069d7455942f8f5d074332bc0bb0f97c4b1
Michael Moncada <michael.moncada@gmail.com>
2021-01-01 00:21:23 -0500

    Add space ship health logic

--- MeteorGame/sprites/Spaceship.py
+++ MeteorGame/sprites/Spaceship.py
@@ -2,12 +2,30 @@ from .Sprite import Sprite
 
 
 class SpaceShip(Sprite):
-    def __init__(self, screen, file_name, x_position, y_position, speed):
+    def __init__(self, screen, file_name, x_position, y_position):
         Sprite.__init__(self, screen, file_name, x_position, y_position)
+        self.shield_surface = Sprite.load_image('shield.png')
+        self.health = 5
 
     def update(self):
         self.rect.center = self._pygame.mouse.get_pos()
         self.apply_screen_constraints()
+        self.draw_health()
+
+    def draw_health(self):
+        offset_y = self.shield_surface.get_height() / 2
+        x_increment = self.shield_surface.get_width() + 10
+        x_pos = 10
+
+        for i in range(self.health):
+            self._screen.blit(self.shield_surface, (x_pos, offset_y))
+            x_pos += x_increment
+
+    def collide(self, health_penality):
+        self.health -= health_penality
+
+        if self.health <= 0:
+            self.kill()
 
     def apply_screen_constraints(self):
         screen_width = self._screen.get_width()
