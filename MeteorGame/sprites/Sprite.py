b067c8a2912801c0395b37f7f4ea5623c81eadb1
Michael Moncada <michael.moncada@gmail.com>
2021-01-01 00:20:31 -0500

    Add static method to load images

--- MeteorGame/sprites/Sprite.py
+++ MeteorGame/sprites/Sprite.py
@@ -6,17 +6,20 @@ class Sprite(pygame.sprite.Sprite):
     ASSETS_DIR = '{0}/assets/img'.format(os.getcwd())
     SURFACE_CACHE = {}
 
-    def __init__(self, screen, file_name, x_position, y_position):
-        pygame.sprite.Sprite.__init__(self)
-        self._pygame = pygame
-        self._screen = screen
-
+    @staticmethod
+    def load_image(file_name):
         image_file_path = '{0}/{1}'.format(Sprite.ASSETS_DIR, file_name)
 
         if image_file_path not in Sprite.SURFACE_CACHE:
             Sprite.SURFACE_CACHE[image_file_path] = pygame.image.load(image_file_path)
 
-        self.image = Sprite.SURFACE_CACHE[image_file_path]
+        return Sprite.SURFACE_CACHE[image_file_path]
+
+    def __init__(self, screen, file_name, x_position, y_position):
+        pygame.sprite.Sprite.__init__(self)
+        self._pygame = pygame
+        self._screen = screen
+        self.image = Sprite.load_image(file_name)
         self.rect = self.image.get_rect(center=(x_position, y_position))
 
     def apply_screen_constraints(self):
