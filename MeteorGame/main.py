c0dc29237dd77e2b37887651ab27a9c7c20d8c78
Michael Moncada <michael.moncada@gmail.com>
2021-01-01 20:26:05 -0500

    Add Sprite to package

--- MeteorGame/main.py
+++ MeteorGame/main.py
@@ -31,18 +31,42 @@ def get_meteors(screen, number_of_meteors, max_reuse_count=1):
     return meteors
 
 
-def show_game_over_screen():
-    pass
+def show_score(screen, font, score, font_colour=(255, 255, 255)):
+    score_label = font.render('Score:'.format(score), True, font_colour, None)
+
+    screen_width = screen.get_width()
+
+    score_label_rect = score_label.get_rect()
+    score_label_rect.topleft = (screen_width - 2.5 * score_label.get_width(), 10)
+
+    score_text = font.render(str(score), True, font_colour, None)
+    score_text_rect = score_text.get_rect()
+    score_text_rect.topleft = (score_label_rect.right + 10, score_label_rect.top)
+
+    screen.blit(score_label, score_label_rect)
+    screen.blit(score_text, score_text_rect)
+
+    return score_text
 
 
-def show_game_start_screen():
-    pass
+def show_game_over_screen(screen, font, font_colour=(255, 255, 255)):
+    text = font.render('Game Over - Press Mouse button to restart...', True, font_colour, None)
+
+    screen_width = screen.get_width()
+    screen_height = screen.get_height()
+
+    text_rect = text.get_rect()
+    text_rect.center = (screen_width / 2, screen_height / 2)
+    screen.blit(text, text_rect)
+
+    return text
 
 
 def start_game(window_width=1200, window_height=720):
     pygame.init()
     screen = pygame.display.set_mode((window_width, window_height))
     clock = pygame.time.Clock()
+    game_font = pygame.font.Font('{0}/LazenbyCompSmooth.ttf'.format(sprites.Sprite.ASSETS_DIR), 30)
 
     spaceship = get_spaceship(screen)
     spaceship_group = pygame.sprite.GroupSingle(spaceship)
@@ -59,12 +83,12 @@ def start_game(window_width=1200, window_height=720):
     pygame.time.set_timer(meteor_event, timer_interval_ms)
 
     game_states = {
-        'START': 1,
         'PLAYING': 2,
         'ENDED': 3
     }
 
     game_state = game_states['PLAYING']
+    game_score = 0
 
     while True:
         for event in pygame.event.get():
@@ -74,8 +98,14 @@ def start_game(window_width=1200, window_height=720):
             elif event.type == meteor_event:
                 meteor_group.add(get_meteors(screen, meteor_count))
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                pos_x, pos_y = pygame.mouse.get_pos()
-                laser_group.add(get_laser(screen, pos_x, pos_y, 15))
+                if game_state == game_states['ENDED']:
+                    game_state = game_states['PLAYING']
+                    spaceship.reset_health()
+                    meteor_group.empty()
+                    laser_group.empty()
+                else:
+                    pos_x, pos_y = pygame.mouse.get_pos()
+                    laser_group.add(get_laser(screen, pos_x, pos_y, 15))
 
         screen.fill((42, 45, 51))
 
@@ -97,16 +127,16 @@ def start_game(window_width=1200, window_height=720):
                     laser.kill()
 
         # Ended Game State
-        elif game_state == game_states['ENDED']:
-            show_game_over_screen()
-
-        # Beginning of Game State
         else:
-            show_game_start_screen()
+            show_game_over_screen(screen, game_font)
 
         if spaceship.health < 0:
             game_state = game_states['ENDED']
 
+        if game_state == game_states['PLAYING']:
+            game_score += 1
+
+        show_score(screen, game_font, game_score)
         pygame.display.update()
         clock.tick(120)
 
--- MeteorGame/sprites/__init__.py
+++ MeteorGame/sprites/__init__.py
@@ -1,3 +1,4 @@
+from .Sprite import Sprite
 from .Spaceship import SpaceShip
 from .Laser import Laser
 from .Meteor import Meteor
